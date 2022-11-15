06b1748a93aa1dcb1cb3329b1430b573d6780078
Julien Boulen <julien.boulen@univ-rennes2.fr>
2022-11-15 22:39:36 +0100

    fix unit tests

--- grade/import/csv/tests/load_data_test.php
+++ grade/import/csv/tests/load_data_test.php
@@ -398,7 +398,8 @@ Bobby,Bunce,,"Moodle HQ","Rock on!",student5@example.com,75.00,,75.00,{exportdat
         $newgrades = $testobject->test_update_grade_item($this->courseid, $map, $key, $verbosescales, 'A', $linenumber);
         // The $newgrades variable should be null.
         $this->assertNull($newgrades);
-        $expectederrormessage = get_string('badgrade', 'grades');
+        $a = array('badgrade' => $value, 'linenumber' => $linenumber);
+        $expectederrormessage = get_string('badgrade', 'gradeimport_csv', $a);
         // Check that the error message is what we expect.
         $gradebookerrors = $testobject->get_gradebookerrors();
         $this->assertEquals($expectederrormessage, $gradebookerrors[0]);
