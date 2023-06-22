ba47fbf1d3663757a63a063306d443e1188f9531
Julien Boulen <julien.boulen@univ-rennes2.fr>
2023-06-22 10:31:01 +0200

    MDL-77137 filelib: honour proxybypass option for multiple Curl queries

--- lib/filelib.php
+++ lib/filelib.php
@@ -3655,6 +3655,10 @@ class curl {
                 $options[$n] = $v;
             }
             $handles[$i] = curl_init($requests[$i]['url']);
+
+            // Set the URL as a curl option.
+            $this->setopt(array('CURLOPT_URL' => $requests[$i]['url']));
+
             $this->apply_opt($handles[$i], $options);
             curl_multi_add_handle($main, $handles[$i]);
         }
